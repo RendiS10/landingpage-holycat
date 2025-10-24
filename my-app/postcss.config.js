8d1d93330c8d68e4acb94db1ee8adca6987fb47f
Rendi Sutendi <130779841+RendiS10@users.noreply.github.com>
2025-10-24 15:42:03 +0700

    Delete my-app directory

--- my-app/postcss.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  plugins: {
-    // 1. Tambahkan Tailwind CSS sebagai plugin PostCSS
-    tailwindcss: {},
-    // 2. Autoprefixer memastikan kompatibilitas CSS di berbagai browser
-    autoprefixer: {},
-  },
-};
